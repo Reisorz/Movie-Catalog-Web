7f0f04497d2b885a6718cf22349a13fcb9c187c9
Reisorz <166374263+Reisorz@users.noreply.github.com>
2024-08-23 10:01:29 +0200

    Working on IndexControlador and index.xhtml (Menubar and Data Table)

---
+++ src/main/java/gm/Movie_catalog/controlador/IndexControlador.java
@@ -0,0 +1,35 @@
+package gm.Movie_catalog.controlador;
+
+import gm.Movie_catalog.modelo.Pelicula;
+import gm.Movie_catalog.servicio.IPeliculaServicio;
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.view.ViewScoped;
+import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Data
+@ViewScoped
+public class IndexControlador {
+
+    @Autowired
+    IPeliculaServicio peliculaServicio;
+    private List<Pelicula> peliculas;
+    private static final Logger logger = LoggerFactory.getLogger(IndexControlador.class);
+
+    @PostConstruct
+    public void init() {
+        cargarDatos();
+    }
+
+    public void cargarDatos() {
+        peliculas = peliculaServicio.listarPeliculas();
+        peliculas.forEach(pelicula -> logger.info(pelicula.toString()));
+    }
+
+}
