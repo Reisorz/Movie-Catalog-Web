51488daf1292da251943789e79955cbf642367b1
Reisorz <166374263+Reisorz@users.noreply.github.com>
2024-08-26 12:37:00 +0200

    Modal to add movies done. guardarPelicula() done.

--- src/main/java/gm/Movie_catalog/controlador/IndexControlador.java
+++ src/main/java/gm/Movie_catalog/controlador/IndexControlador.java
@@ -3,8 +3,11 @@ package gm.Movie_catalog.controlador;
 import gm.Movie_catalog.modelo.Pelicula;
 import gm.Movie_catalog.servicio.IPeliculaServicio;
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import lombok.Data;
+import org.primefaces.PrimeFaces;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +38,23 @@ public class IndexControlador {
 
     public void agregarPelicula() {
         this.peliculaSeleccionada = new Pelicula();
+    }
+
+    public void guardarPelicula(){
+        logger.info("pelicula a guardar: " + this.peliculaSeleccionada);
+        //Agregar
+        if(this.peliculaSeleccionada.getIdPelicula() == null){
+            this.peliculaServicio.guardarPelicula(this.peliculaSeleccionada);
+            this.peliculas.add(this.peliculaSeleccionada);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Pelicula Agregada"));
+        }
+        //Ocultar la ventana modal
+        PrimeFaces.current().executeScript("PF('ventanaModalPelicula').hide()");
+        //Actualizar la tabla usando ajax
+        PrimeFaces.current().ajax().update("forma-peliculas:mensajes",
+                "forma-peliculas:peliculas-tabla");
+        //Reset del objeto pelicula seleccionada
+        this.peliculaSeleccionada = null;
     }
 
 }
